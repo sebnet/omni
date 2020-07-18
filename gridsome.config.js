6dfd761084cfe179efc183f27767274d90b7efbe
sebnet <sebodena@gmail.com>
2020-07-18 20:34:17 -0300

    .

--- gridsome.config.js
+++ gridsome.config.js
@@ -18,6 +18,20 @@ module.exports = {
 
   plugins: [
     {
+      use: 'gridsome-plugin-purgecss',
+      // default options, the following will be included if you don't provide anything
+      options: {
+        content: [
+          './src/**/*.vue',
+          './src/**/*.js',
+          './src/**/*.jsx',
+          './src/**/*.pug',
+          './src/**/*.md',
+        ],
+        defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || [],
+      },
+    },
+    {
       use: '@gridsome/source-filesystem',
       options: {
         path: './src/data/productos/*.md',
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,30 @@
 module.exports = {
+  hainWebpack: (config) => {
+    config.module
+      .rule('postcss') // css, sass, scss, less, postcss, stylus
+      .oneOf('normal') // normal, module
+      .use('postcss-loader')
+      .tap((options) => {
+        options.plugins.unshift(
+          ...[
+            require('postcss-import'),
+            require('tailwindcss')(config.tailwind),
+          ],
+        );
+
+        if (process.env.NODE_ENV === 'production') {
+          options.plugins.push(
+            ...[require('@fullhuman/postcss-purgecss')(config.purgecss)],
+          );
+        }
+
+        return options;
+      });
+  },
   purge: [],
   theme: {
     extend: {},
   },
   variants: {},
   plugins: [],
-}
+};
