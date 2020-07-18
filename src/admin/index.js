26d4cae9a473410252b490d89eae24dd2e7e094c
sebnet <sebodena@gmail.com>
2020-07-18 16:21:54 -0300

    .

--- src/admin/index.js
+++ src/admin/index.js
@@ -1,1 +1,4 @@
 import CMS from 'netlify-cms';
+import { es } from 'netlify-cms-locales';
+
+CMS.registerLocale('es', es);
--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,18 +1,8 @@
 <template>
   <div class="layout">
-    <header class="header">
-      <strong>
-        <g-link to="/">{{ $static.metadata.siteName }}</g-link>
-      </strong>
-      <nav class="nav">
-        <g-link class="nav__link" to="/">Home</g-link>
-        <g-link class="nav__link" to="/about/">About</g-link>
-      </nav>
-    </header>
-    <slot/>
+    <slot />
   </div>
 </template>
-
 <static-query>
 query {
   metadata {
@@ -20,31 +10,17 @@ query {
   }
 }
 </static-query>
-
 <style>
 body {
-  font-family: -apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif;
-  margin:0;
-  padding:0;
-  line-height: 1.5;
+  background-color: #f4f4f4;
+  padding: 0;
+  margin: 0;
 }
-
-.layout {
-  max-width: 760px;
-  margin: 0 auto;
-  padding-left: 20px;
-  padding-right: 20px;
-}
-
-.header {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  margin-bottom: 20px;
-  height: 80px;
-}
-
-.nav__link {
-  margin-left: 20px;
+@media screen and (min-width: 992px) {
+  .container {
+    width: 60%;
+    margin: 0 auto;
+    padding: 30px 0px;
+  }
 }
 </style>
